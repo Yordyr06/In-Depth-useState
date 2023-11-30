8fcf3106722fa57e4a28f84bf69f740e8439b8af
Yordy Almonte <yoalmonte.dev@outlook.com>
2023-11-30 01:17:16 -0400

    ⚙️ - Set project config

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+export const App = (): JSX.Element => {
+  return (
+    <>
+      <h1 className="font-extrabold text-4xl text-transparent bg-clip-text bg-gradient-to-r from-slate-500 to-white">
+        My App
+      </h1>
+    </>
+  )
+}
+
+export default App
